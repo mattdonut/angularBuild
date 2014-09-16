c93823114809edcca1a94b6839d60a14f11cdbcc
Matthew Brunner <matt.donut.brunner@gmail.com>
2014-09-16 14:57:40 -0700

    Update server.go
    Added a comment, not really necessary

--- server.go
+++ server.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// This is the main serving entry point
+
 func main() {
 	// Simple static webserver:
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./build"))))
